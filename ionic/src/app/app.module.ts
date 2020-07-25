18db90fb0cda1b4bf737b96f7f3800523df4c257
Shu Hui Han <hanshuh@cn.ibm.com>
2020-07-25 16:46:20 +0800

    first commit

---
+++ ionic/src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { DataStore } from './dataStore';
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+import { LiveUpdateProvider } from '../providers/live-update/live-update';
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp),
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    DataStore,
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    LiveUpdateProvider
+  ]
+})
+export class AppModule {}
